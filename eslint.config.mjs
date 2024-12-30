386e1f3be092c6d3014e6800ba6030a598a1e7f3
TheGoldProject <thegoldproject726@gmail.com>
2024-12-30 15:23:21 +0900

    update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,24 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      // Add your custom rules here
+      // "no-console": "warn", // Warn when `console.log` is used
+      // "semi": ["error", "always"], // Enforce semicolons
+      // "quotes": ["error", "single"], // Enforce single quotes
+      "@typescript-eslint/no-unused-vars": "off", // Ignore unused variables starting with `_`
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-empty-object-type": "off",
+      "prefer-const": "off",
+      "react-hooks/exhaustive-deps": "off",
+      "react/no-unescaped-entities": "off",
+      "@typescript-eslint/no-unused-expressions": "off",
+      "@typescript-eslint/ban-ts-comment": "off",
+    },
+  },
+];
 
 export default eslintConfig;
